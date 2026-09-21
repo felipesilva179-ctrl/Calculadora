61ebe77a3bf67b60739a8e39a26a936fc91c957e
Felipe Dantas <felipesilva179@gmail.com>
2026-09-21 12:52:10 -0300

    Adiciona método subtract e teste

--- src/test/java/calculadora/CalculatorTest.java
+++ src/test/java/calculadora/CalculatorTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
+	
+	   @Test
+	    void testSubtract() {
+	        assertEquals(2, Calculator.subtract(5, 3));
+	    }
 
     @Test
     public void testAdd() {
